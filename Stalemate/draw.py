51088a04a43fd811336ccc9314a054fc77713ac4
QLabs <55121845+Quantalabs@users.noreply.github.com>
2020-10-19 19:47:07 -0700

    In-Progress

--- Stalemate/draw.py
+++ Stalemate/draw.py
@@ -1,8 +1,26 @@
 from tkinter import *
 
+board = [["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜",
+     "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟",
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     "♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙",
+     "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
+     ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜",
+     "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟",
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, "♙", 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     "♙", "♙", "♙", "♙", 0, "♙", "♙", "♙",
+     "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]]
+
+
 def draw(board):
     root = Tk()
-    root.title('Stalemates')
+    root.title('Stalemate')
     c = Canvas(width=800, height=800)
     c.pack()
     root.geometry('800x800')
@@ -35,3 +53,90 @@ def draw(board):
                 piece = c.create_text(50 * y + 200 - 25, 200 + 25 + x * 50 - 50, text=board[(x - 1) * 8 + y - 1],
                                       font=("Helvetica", 30))
     root.mainloop()
+
+
+def game(game, moves=10):
+    root = Tk()
+    root.title('Stalemate')
+    c = Canvas(width=800, height=800)
+    c.pack()
+    root.geometry('900x1600')
+
+    scene = 0
+
+    for board in game[scene]:
+        for x in range(1, 9):
+            if x % 2 == 0:
+                board_square = c.create_rectangle(50 * x + 200 - 50, 200, 50 * x + 200, 250, fill='white')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 250, 50 * x + 200, 300, fill='brown')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 300, 50 * x + 200, 350, fill='white')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 350, 50 * x + 200, 400, fill='brown')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 400, 50 * x + 200, 450, fill='white')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 450, 50 * x + 200, 500, fill='brown')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 500, 50 * x + 200, 550, fill='white')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 550, 50 * x + 200, 600, fill='brown')
+            else:
+                board_square = c.create_rectangle(50 * x + 200 - 50, 200, 50 * x + 200, 250, fill='brown')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 250, 50 * x + 200, 300, fill='white')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 300, 50 * x + 200, 350, fill='brown')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 350, 50 * x + 200, 400, fill='white')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 400, 50 * x + 200, 450, fill='brown')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 450, 50 * x + 200, 500, fill='white')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 500, 50 * x + 200, 550, fill='brown')
+                board_square = c.create_rectangle(50 * x + 200 - 50, 550, 50 * x + 200, 600, fill='white')
+        for x in range(1, 9):
+            for y in range(1, 9):
+                if board[(x - 1) * 8 + y - 1] == 0:
+                    continue
+                else:
+                    piece = c.create_text(50 * y + 200 - 25, 200 + 25 + x * 50 - 50,
+                                          text=board[(x - 1) * 8 + y - 2],
+                                          font=("Helvetica", 30))
+
+    def redraw(game):
+        for board in game[scene]:
+            for x in range(1, 9):
+                if x % 2 == 0:
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 200, 50 * x + 200, 250, fill='white')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 250, 50 * x + 200, 300, fill='brown')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 300, 50 * x + 200, 350, fill='white')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 350, 50 * x + 200, 400, fill='brown')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 400, 50 * x + 200, 450, fill='white')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 450, 50 * x + 200, 500, fill='brown')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 500, 50 * x + 200, 550, fill='white')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 550, 50 * x + 200, 600, fill='brown')
+                else:
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 200, 50 * x + 200, 250, fill='brown')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 250, 50 * x + 200, 300, fill='white')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 300, 50 * x + 200, 350, fill='brown')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 350, 50 * x + 200, 400, fill='white')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 400, 50 * x + 200, 450, fill='brown')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 450, 50 * x + 200, 500, fill='white')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 500, 50 * x + 200, 550, fill='brown')
+                    board_square = c.create_rectangle(50 * x + 200 - 50, 550, 50 * x + 200, 600, fill='white')
+            for x in range(1, 9):
+                for y in range(1, 9):
+                    if board[(x - 1) * 8 + y - 2] == 0:
+                        continue
+                    else:
+                        piece = c.create_text(50 * y + 200 - 25, 200 + 25 + x * 50 - 50,
+                                              text=board[(x - 1) * 8 + y - 1],
+                                              font=("Helvetica", 30))
+
+    def push_scene(scene_var):
+        if scene+1 > moves:
+            scene_var += 1
+        else:
+            pass
+
+    def Next(scene_var, board):
+        push_scene(scene_var)
+        redraw(board[scene_var])
+        root.canvas.update_idletasks()
+
+    nextButton = Button(root, text="Next", command=Next(scene, game))
+    nextButton.place(x=850, y=900)
+
+    root.mainloop()
+
+game(board,moves=2)
